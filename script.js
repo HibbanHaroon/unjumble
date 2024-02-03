046fc7f8319714f6ab85f21d8b2bce38ceeef031
HibbanHaroon <hibbanharoon@gmail.com>
2024-02-03 16:50:52 +0500

    Change APIs and Add Settings

--- script.js
+++ script.js
@@ -1,72 +1,129 @@
-// Initializing with them with default values just in case
-let word = "EPIPHANY";
-let hint = "HINT: A sudden realization or understanding.";
+let word = "";
+let wordLength = 5;
+let noOfWords = 1;
+let isSwears = false;
+let hint = "Not available right now pookie";
 
-function checkEnter(event) {
-  if (event.key === "Enter") {
-      compareWord();
+let settingsClicked = false;
+
+function displaySettings() {
+  settingsClicked = !settingsClicked;
+  var dropdownElement = document.querySelector(".dropdown");
+  if (settingsClicked) {
+    dropdownElement.classList.add("unhide");
+  } else {
+    dropdownElement.classList.remove("unhide");
   }
 }
 
-function compareWord(){
-    if(document.getElementById('checkButton')){
-        enteredWord = document.getElementById('text').value;
-        if(enteredWord.toUpperCase().trim() === word){
-          animation.innerHTML = '<img src="assets/tick.gif" alt="Tick Animation">';
-          document.getElementById("text").value = "";
-          windowsOnLoad();
-        }
-        else{
-          animation.innerHTML = '<img src="assets/cross.gif" alt="Cross Animation">';
-        }
-    }
+function settingsChanged() {
+  wordLength = document.getElementById("wordLength").value;
+  isSwears = document.getElementById("swears").checked;
+  displaySettings();
+  windowsOnLoad();
+}
 
-    setTimeout(() => {
-      animation.innerHTML = '';
+function checkEnter(event) {
+  if (event.key === "Enter") {
+    compareWord();
+  }
+}
+
+function compareWord() {
+  if (document.getElementById("checkButton")) {
+    enteredWord = document.getElementById("text").value;
+    if (enteredWord.toUpperCase().trim() === word.toUpperCase().trim()) {
+      animation.innerHTML = '<img src="assets/tick.gif" alt="Tick Animation">';
+      document.getElementById("hint").textContent = "Hint : ";
       document.getElementById("text").value = "";
+      windowsOnLoad();
+    } else {
+      animation.innerHTML =
+        '<img src="assets/cross.gif" alt="Cross Animation">';
+    }
+    console.log(word);
+    console.log(enteredWord);
+  }
+
+  setTimeout(() => {
+    animation.innerHTML = "";
+    document.getElementById("text").value = "";
   }, 1800);
 }
 
-function generateUnjumbledWord(word){
+function generateUnjumbledWord(word) {
   let jumbledWord = word;
-  for(var i = 0; i < jumbledWord.length; i++){
-      let randomIndex = Math.floor(Math.random() * 5);
-      let temp = jumbledWord[i];
-      jumbledWord = jumbledWord.substring(0, i) + jumbledWord.substring(randomIndex, randomIndex + 1) + jumbledWord.substring(i + 1)
-      jumbledWord = jumbledWord.substring(0, randomIndex) + temp + jumbledWord.substring(randomIndex + 1)
+  for (var i = 0; i < jumbledWord.length; i++) {
+    let randomIndex = Math.floor(Math.random() * 5);
+    let temp = jumbledWord[i];
+    jumbledWord =
+      jumbledWord.substring(0, i) +
+      jumbledWord.substring(randomIndex, randomIndex + 1) +
+      jumbledWord.substring(i + 1);
+    jumbledWord =
+      jumbledWord.substring(0, randomIndex) +
+      temp +
+      jumbledWord.substring(randomIndex + 1);
   }
   return jumbledWord;
 }
 
-function windowsOnLoad(){
-// Make a GET request to the API endpoint
-fetch('https://unjumble-api.onrender.com/')
-  .then(response => {
-    // Check if the response status is OK (200)
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    // Parse the response JSON
-    return response.json();
-  })
-  .then(data => {
-    // Access the 'word' and 'hint' fields from the JSON response
-    word = data.word.toUpperCase();
-    hint = data.hint;    
+function removePunctuation(word) {
+  const punctuationRegex = /[!#$%&()*+-.:;<=>?@[\]^_`{|}~]/g;
 
-    document.getElementById('word').textContent = generateUnjumbledWord(word).toUpperCase();
-    document.getElementById('hint').textContent = "HINT: " + hint;
+  const cleanedWord = word.replace(punctuationRegex, "");
 
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+  return cleanedWord;
+}
+
+function windowsOnLoad() {
+  fetchWord();
+  fetchHint();
+}
+
+function fetchWord() {
+  fetch(
+    `https://unjumble-word-api.onrender.com/?wordLength=${wordLength}&noOfWords=${noOfWords}&isSwears=${isSwears}`
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      word = data.words[0];
+
+      document.getElementById("word").textContent =
+        generateUnjumbledWord(word).toUpperCase();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function fetchHint() {
+  fetch(`https://unjumble-api.onrender.com/?word=${word}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      hint = data.hint;
+      hint = removePunctuation(hint);
+
+      document.getElementById("hint").textContent = "Hint : " + hint;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
 }
 
 window.addEventListener("load", () => {
   windowsOnLoad();
 
-  // Delay for 4 seconds (4000 milliseconds)
   setTimeout(() => {
     document.querySelector(".loader").classList.add("loader--hidden");
     document.querySelector(".loader").addEventListener("transitionend", () => {
